d1eb7d8824904a566c153000ec1597ee5c439189
MatheusBenetti <mbbenetti1@gmail.com>
2022-09-14 12:04:09 -0300

    create list and get room methods

--- chatapp/room/views.py
+++ chatapp/room/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Room
 
-# Create your views here.
+
+@login_required
+def list_rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+
+
+@login_required
+def get_room(request, slug):
+    room = Room.objects.get(slug=slug)
+
+    return render(request, 'room/room.html', {'room': room})
