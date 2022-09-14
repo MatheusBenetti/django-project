119ce395de9c663c7a73588587cff99ded6249cc
MatheusBenetti <mbbenetti1@gmail.com>
2022-09-14 16:07:35 -0300

    import message model to view

--- chatapp/room/views.py
+++ chatapp/room/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Room
+from .models import Message, Room
 
 
 @login_required
@@ -13,5 +13,6 @@ def list_rooms(request):
 @login_required
 def get_room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
